2eefed864d02752d60c435e7746b794ac66eb844
Olivier Archambault <oarchambault@gmail.com>
2022-12-06 10:48:01 +0100

    solution day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,36 @@
+﻿var input = File.ReadAllText("input.txt");
+
+Console.WriteLine("Part 1");
+GetMarkerPosition("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4);
+GetMarkerPosition("bvwbjplbgvbhsrlpgdmjqwftvncz", 4);
+GetMarkerPosition("nppdvjthqldpwncqszvftbrmjlhg", 4);
+GetMarkerPosition("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4);
+GetMarkerPosition("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4);
+GetMarkerPosition(input, 4);
+
+Console.WriteLine();
+Console.WriteLine("Part 2");
+GetMarkerPosition("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14);
+GetMarkerPosition("bvwbjplbgvbhsrlpgdmjqwftvncz", 14);
+GetMarkerPosition("nppdvjthqldpwncqszvftbrmjlhg", 14);
+GetMarkerPosition("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14);
+GetMarkerPosition("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14);
+GetMarkerPosition(input, 14);
+
+
+static int GetMarkerPosition(string input, int markerLenght)
+{
+    for (int index = 0; index < input.Length - (markerLenght + 1); index++)
+    {
+        var substring = input.Substring(index, markerLenght);
+        if (CharsAreDistinct(substring))
+        {
+            Console.WriteLine($"Found {substring} at {index + markerLenght}");
+            return index;
+        }
+    }
+
+    return -1;
+}
+
+static bool CharsAreDistinct(string value) => value.Distinct().Count() == value.Length;
