8eb2d935f1ed76f9ea03f61c4c27321ac6e6ee49
Olivier Archambault <oarchambault@gmail.com>
2022-12-08 18:39:08 +0100

    Day 8 part 2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -4,6 +4,9 @@ var maxRowIndex = treeInput.Length;
 var maxColIndex = treeInput[0].Length;
 
 var visibleTrees = new HashSet<Tree>();
+var maxScenicScore = -1;
+var maxScenicRow = -1;
+var maxScenicCol = -1;
 
 var trees = treeInput
     .Select((row, rowIndex) =>
@@ -15,16 +18,41 @@ for (int rowIndex = 0; rowIndex < maxRowIndex; rowIndex++)
     var treeRow = GetTreeRow(trees, rowIndex, 0, maxColIndex);
     visibleTrees.UnionWith(GetVisibleTrees(treeRow));
     visibleTrees.UnionWith(GetVisibleTrees(treeRow.Reverse()));
-}
 
-for (int colIndex = 0; colIndex < maxColIndex; colIndex++)
-{
-    var treeColumn = GetTreeColumn(trees, colIndex, 0, maxRowIndex);
-    visibleTrees.UnionWith(GetVisibleTrees(treeColumn));
-    visibleTrees.UnionWith(GetVisibleTrees(treeColumn.Reverse()));
+    for (int colIndex = 0; colIndex < maxColIndex; colIndex++)
+    {
+        if (rowIndex == 0)
+        {
+            var treeColumn = GetTreeColumn(trees, colIndex, 0, maxRowIndex);
+            visibleTrees.UnionWith(GetVisibleTrees(treeColumn));
+            visibleTrees.UnionWith(GetVisibleTrees(treeColumn.Reverse()));
+        }
+
+        var treeColumnUp = GetTreeColumn(trees, colIndex, 0, rowIndex + 1).Reverse();
+        var treeColumnDown = GetTreeColumn(trees, colIndex, rowIndex, maxRowIndex);
+        var treeRowLeft = GetTreeRow(trees, rowIndex, 0, colIndex + 1).Reverse();
+        var treeRowRight = GetTreeRow(trees, rowIndex, colIndex, maxColIndex);
+
+        var visibleTreesUp = GetNumberOfVisibleTreesFromFirst(treeColumnUp);
+        var visibleTreesDown = GetNumberOfVisibleTreesFromFirst(treeColumnDown);
+        var visibleTreesLeft = GetNumberOfVisibleTreesFromFirst(treeRowLeft);
+        var visibleTreesRight = GetNumberOfVisibleTreesFromFirst(treeRowRight);
+
+        var scenicScore = visibleTreesUp * visibleTreesDown * visibleTreesLeft * visibleTreesRight;
+        if(scenicScore > maxScenicScore)
+        {
+            maxScenicScore = scenicScore;
+            maxScenicRow = rowIndex;
+            maxScenicCol = colIndex;
+        }
+    }
+
 }
 
 Console.WriteLine($"We found {visibleTrees.Count} visible trees.");
+Console.WriteLine();
+
+Console.WriteLine($"The max scenic score is {maxScenicScore} at ({maxScenicRow}, {maxScenicCol}).");
 Console.WriteLine();
 
 static IEnumerable<Tree> GetTreeRow(Tree[][] trees, int rowIndex, int startColIndex, int endColIndex)
@@ -34,7 +62,9 @@ static IEnumerable<Tree> GetTreeRow(Tree[][] trees, int rowIndex, int startColIn
 
 static IEnumerable<Tree> GetTreeColumn(Tree[][] trees, int colIndex, int startRowIndex, int endRowIndex)
 {
-    return Enumerable.Range(startRowIndex, endRowIndex).Select(rowIndex => trees[rowIndex][colIndex]);
+    return Enumerable
+        .Range(startRowIndex, Math.Abs(startRowIndex - endRowIndex))
+        .Select(rowIndex => trees[rowIndex][colIndex]);
 }
 
 IEnumerable<Tree> GetVisibleTrees(IEnumerable<Tree> treeLine)
@@ -42,12 +72,27 @@ IEnumerable<Tree> GetVisibleTrees(IEnumerable<Tree> treeLine)
     var maxHeight = -1;
     foreach (Tree tree in treeLine)
     {
-        if (tree.height > maxHeight)
+        if (tree.Height > maxHeight)
         {
-            maxHeight = tree.height;
+            maxHeight = tree.Height;
             yield return tree;
         }
     }
 }
 
-record Tree(int height, int row, int column);
+int GetNumberOfVisibleTreesFromFirst(IEnumerable<Tree> treeLine)
+{
+    var height = treeLine.First().Height;
+    var count = 0;
+    foreach (Tree tree in treeLine.Skip(1))
+    {
+        count++;
+        if (tree.Height >= height)
+        {
+            break;
+        }
+    }
+    return count;
+}
+
+record Tree(int Height, int Row, int Column);
