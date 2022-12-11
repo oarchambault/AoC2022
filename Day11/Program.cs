0e44114a97c8e0336d4f7cd525964d1facf2909b
Olivier Archambault <oarchambault@gmail.com>
2022-12-11 18:10:20 +0100

    Day 11 part 1

---
+++ Day11/Program.cs
@@ -0,0 +1,113 @@
+﻿Logger.LogEnabled = false;
+
+var monkeysNotes = File.ReadAllText("input.txt");
+
+var monkeys = monkeysNotes.Split("\r\n\r\n").Select(n => Monkey.FromNotes(n)).ToArray();
+
+const int numberOfRounds = 20;
+
+for (int round = 0; round < numberOfRounds; round++)
+{
+    foreach (var monkey in monkeys)
+    {
+        monkey.ProceedRound(monkeys);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine($"After round {round + 1}, the monkeys are holding items with these worry levels:");
+    foreach (var monkey in monkeys)
+    {
+        Console.WriteLine($"{monkey.Name}: {string.Join(", ", monkey.Items)}");
+    }
+
+    Console.WriteLine();
+    //Console.ReadKey();
+}
+
+Console.WriteLine();
+foreach (var monkey in monkeys)
+{
+    Console.WriteLine($"{monkey.Name} inspected items {monkey.InspectionCount} times.");
+}
+
+var mostActiveMonkeys = monkeys.OrderByDescending(m => m.InspectionCount).Take(2).ToList();
+Console.WriteLine($"Most active monkeys are {mostActiveMonkeys[0].Name} and {mostActiveMonkeys[1].Name}");
+Console.WriteLine($"The monkey business level is {mostActiveMonkeys[0].InspectionCount * mostActiveMonkeys[1].InspectionCount}");
+
+
+
+class Monkey
+{
+    public string Name { get; private set; } = string.Empty;
+    public IList<long> Items { get; private set; } = Array.Empty<long>();
+    public Func<long, long> WorryOperation { get; private set; } = (old) => old;
+    public int TestDivisibleby { get; private set; }
+    public int TestTrueThrowTo { get; private set; }
+    public int TestFalseThrowTo { get; private set; }
+
+    public int InspectionCount { get; set; }
+
+    private Monkey() { }
+
+    public static Monkey FromNotes(string monkeyNotes)
+    {
+        var lines = monkeyNotes.Split("\r\n");
+
+        var monkey = new Monkey();
+
+        monkey.Name = lines[0].TrimEnd(':');
+        monkey.Items = lines[1].Split(':')[1].Split(", ").Select(long.Parse).ToList();
+
+        var operationNote = lines[2].Split("= old ")[1].Split(' ');
+        monkey.WorryOperation = operationNote switch
+        {
+            ["*", "old"] => (long old) => old * old,
+            ["*", string value] => (long old) => old * long.Parse(value),
+            ["+", string value] => (long old) => old + long.Parse(value),
+            _ => throw new ArgumentException(nameof(operationNote)),
+        };
+
+        monkey.TestDivisibleby = int.Parse(lines[3].Split(' ').Last());
+        monkey.TestTrueThrowTo = int.Parse(lines[4].Split(' ').Last());
+        monkey.TestFalseThrowTo = int.Parse(lines[5].Split(' ').Last());
+
+        return monkey;
+    }
+
+    public void ProceedRound(Monkey[] allMonkeys)
+    {
+        Logger.Log($"{Name}:");
+
+        for (int i = 0; i < Items.Count; i++)
+        {
+            InspectionCount++;
+            Logger.Log($"  Monkey inspects an item with a worry level of {Items[i]}.");
+            Items[i] = WorryOperation(Items[i]);
+            Logger.Log($"    Worry level increased to {Items[i]}.");
+            Items[i] = Items[i] / 3;
+            Logger.Log($"    Monkey gets bored with item. Worry level is divided by 3 to {Items[i]}.");
+
+            var testResult = Items[i] % TestDivisibleby == 0;
+            var newMonkey = testResult ? TestTrueThrowTo : TestFalseThrowTo;
+            Logger.Log($"    Current worry level is {(testResult ? "" : "not")} divisible by {TestDivisibleby}.");
+            Logger.Log($"    Item with worry level {Items[i]} is thrown to monkey {newMonkey}.");
+            allMonkeys[newMonkey].Items.Add(Items[i]);
+        }
+
+        Items.Clear();
+    }
+}
+
+static class Logger
+{
+    public static bool LogEnabled = true;
+
+    public static void Log(string message = "")
+    {
+        if (LogEnabled)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+
